876ab8cf85f1e8050274d651ef6d32a2b6bce62f
NaskerFo4 <xamps23@gmail.com>
2026-06-05 14:31:03 +0200

    Replace boolean with bool in RTPNeoTrellis and RTPRotaryClickChordion

--- include/Devices/RTPRotaryClickChordion.hpp
+++ include/Devices/RTPRotaryClickChordion.hpp
@@ -7,7 +7,7 @@
 class RTPMainUnit;
 class RTPRotaryClickDev: public RTPRotaryClick{
 public:
-  RTPRotaryClickDev(int leftRotPin, int rightRotPin, int buttonPin, boolean active, boolean pullup):RTPRotaryClick(leftRotPin, rightRotPin, buttonPin, active, pullup){};
+  RTPRotaryClickDev(int leftRotPin, int rightRotPin, int buttonPin, bool active, bool pullup):RTPRotaryClick(leftRotPin, rightRotPin, buttonPin, active, pullup){};
   void callbackFromRotary(RTPMainUnit*);
   void callbackFromClicks(RTPMainUnit*);
 };
